99cfeaf8bf681cfa00b2be05ee34175d8b2705bc
Thomas Stensitzki <thomas@mcsmemail.de>
2017-04-06 10:29:31 +0200

    1.2 | Minor PowerShell changes

--- Get-MailboxPermissionReport.ps1
+++ Get-MailboxPermissionReport.ps1
@@ -1,55 +1,56 @@
 <#
-    .SYNOPSIS
-    Dump mailbox folder permissions to CSV file
+  .SYNOPSIS
+  Dump mailbox folder permissions to CSV file
    
-   	Thomas Stensitzki
+  Thomas Stensitzki
 	
-	THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
-	RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+  THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
+  RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
 	
-	Version 1.1, 2016-08-30
+  Version 1.2, 2017-04-06
 
-    Ideas, comments and suggestions to support@granikos.eu 
+  Ideas, comments and suggestions to support@granikos.eu 
 
-    This script is based on Mr Tony Redmonds blog post http://thoughtsofanidlemind.com/2014/09/05/reporting-delegate-access-to-exchange-mailboxes/ 
+  This script is based on Mr Tony Redmonds blog post http://thoughtsofanidlemind.com/2014/09/05/reporting-delegate-access-to-exchange-mailboxes/ 
  
-    .LINK  
-    More information can be found at http://www.granikos.eu/en/scripts 
+  .LINK  
+  More information can be found at http://www.granikos.eu/en/scripts 
 	
-    .DESCRIPTION
-    This script exports all mailbox folder permissions for mailboxes of type "UserMailbox".
+  .DESCRIPTION
+  This script exports all mailbox folder permissions for mailboxes of type "UserMailbox".
     
-    The permissions are exported to a local CSV file
+  The permissions are exported to a local CSV file
 
-    The script is inteded to run from within an active Exchange 2013 Management Shell session.
+  The script is inteded to run from within an active Exchange 2013 Management Shell session.
 
-    .NOTES 
-    Requirements 
-    - Windows Server 2012 or Windows Server 2012 R2  
+  .NOTES 
+  Requirements 
+  - Windows Server 2012 or Windows Server 2012 R2  
 
-    Revision History 
-    -------------------------------------------------------------------------------- 
-    1.0     Initial community release
-    1.1     Minor PowerShell fix
+  Revision History 
+  -------------------------------------------------------------------------------- 
+  1.0 | Initial community release
+  1.1 | Minor PowerShell fix
+  1.2 | Minor PowerShell changes
 	
-	.PARAMETER CsvFileName
-    CSV file name 
+  .PARAMETER CsvFileName
+  CSV file name 
 
-	.EXAMPLE
-    Export mailbox permissions to export.csv
+  .EXAMPLE
+  Export mailbox permissions to export.csv
 
-    .\Get-MailboxPermissionsReport-ps1 -CsvFileName export.csv
+  .\Get-MailboxPermissionsReport-ps1 -CsvFileName export.csv
 
 #>
+[CmdletBinding()]
 Param(
-    [parameter(Mandatory=$false,ValueFromPipeline=$false,HelpMessage='CSV file name')]
-        [string]$CsvFileName = 'MailboxPermissions.csv'
+  [string]$CsvFileName = 'MailboxPermissions.csv'
 )
 
-$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$ScriptDir = Split-Path -Path $script:MyInvocation.MyCommand.Path
 $ScriptName = $MyInvocation.MyCommand.Name
 
-$OutputFile = Join-Path $ScriptDir -ChildPath $CsvFileName
+$OutputFile = Join-Path -Path $ScriptDir -ChildPath $CsvFileName
 
 Write-Verbose $OutputFile
 
@@ -64,10 +65,10 @@ $count = 1
 
 ForEach ($Mailbox in $Mailboxes) { 
     $Alias = '' + $Mailbox.Name
-    $DisplayName = "$($Mailbox.DisplayName) ($($Mailbox.Name))"
+    $DisplayName = ('{0} ({1})' -f $Mailbox.DisplayName, $Mailbox.Name)
 
-    $activity = "Working... [$($count)/$($mailboxCount)]"
-    $status = "Getting folders for mailbox: $($DisplayName)"
+    $activity = ('Working... [{0}/{1}]' -f $count, $mailboxCount)
+    $status = ('Getting folders for mailbox: {0}' -f $DisplayName)
     Write-Progress -Status $status -Activity $activity -PercentComplete (($count/$MailboxCount)*100) 
     
     # Fetch fodlers
